161839a0d1037dc62b60604f3c28937b18cf8e52
cloudhao1999 <cyh19991002@gmail.com>
2022-05-17 11:50:08 +0800

    docs: 修改文件定义内容
    n

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center">
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <el-button>你好</el-button>
+    <el-button>你好啊</el-button>
   </div>
 </template>
