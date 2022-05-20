7507adf4bcaeb8ee3fb1cbee9ac941804789a2cd
cloudhao1999 <cyh19991002@gmail.com>
2022-05-20 11:44:15 +0800

    style: 标题调整'=

--- src/layouts/default/banner/index.vue
+++ src/layouts/default/banner/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex align-center items-center py-2 w-[200px]">
+  <div class="flex align-center items-center py-2 w-[220px]">
     <div class="w-10">
       <img src="../../../assets/img/logo.png" alt="logo" />
     </div>
     <div class="flex-1">
-      <span class="text-lg font-bold">云App管理系统</span>
+      <span class="text-lg font-bold">云喵喵App管理系统</span>
     </div>
   </div>
 </template>
