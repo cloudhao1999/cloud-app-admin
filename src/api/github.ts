9e7cd31831e7d9a29a83fb0702cc6132898462a9
cloudhao1999 <cyh19991002@gmail.com>
2022-05-26 18:09:48 +0800

    feat: 时间线组件连接Github Api库，获取最新的Commit数据

---
+++ src/api/github.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { BasicGetResult } from "#/resultType";
+
+export type GithubCommitResultType = {
+  commit: {
+    author: {
+      date: string;
+    };
+    message: string;
+  };
+};
+
+/**
+ *
+ * @description 获取仓库commit信息
+ */
+export const fetchCommits = (user: string, repo: string, token: string) => {
+  const url = `https://api.github.com/repos/${user}/${repo}/commits`;
+  return new Promise<BasicGetResult<GithubCommitResultType[]>>(async (resolve, reject) => {
+    try {
+      const res = await axios.get<{}, BasicGetResult<GithubCommitResultType[]>>(url, {
+        headers: {
+          // token30天需要更换一次
+          Authorization: "Bearer " + token
+        }
+      });
+      resolve(res);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
--- src/enum/userEnum.ts
+++ src/enum/userEnum.ts
@@ -1,4 +1,7 @@
 export enum UserEnum {
   GITHUB_REPOSITORY = "https://github.com/cloudhao1999/cloud-app-admin",
-  TOKEN_KEY = "cloud-auth-token"
+  TOKEN_KEY = "cloud-auth-token",
+  GITHUB_USER = "cloudhao1999",
+  GITHUB_REPO = "cloud-app-admin",
+  GITHUB_ACCESS_TOKEN = "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"
 }
