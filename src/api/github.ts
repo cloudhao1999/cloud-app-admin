edc64a8aab28c594ae85142563fb253d3d393aa1
cloudhao1999 <cyh19991002@gmail.com>
2022-05-26 18:13:32 +0800

    fix: 更新token

--- src/api/github.ts
+++ src/api/github.ts
@@ -15,7 +15,7 @@ export type GithubCommitResultType = {
  * @description 获取仓库commit信息
  */
 export const fetchCommits = (user: string, repo: string, token: string) => {
-  const url = `https://api.github.com/repos/${user}/${repo}/commits`;
+  const url = `https://api.github.com/repos/${user}/${repo}/commits?access_token=${token}`;
   return new Promise<BasicGetResult<GithubCommitResultType[]>>(async (resolve, reject) => {
     try {
       const res = await axios.get<{}, BasicGetResult<GithubCommitResultType[]>>(url, {
--- src/enum/userEnum.ts
+++ src/enum/userEnum.ts
@@ -3,5 +3,5 @@ export enum UserEnum {
   TOKEN_KEY = "cloud-auth-token",
   GITHUB_USER = "cloudhao1999",
   GITHUB_REPO = "cloud-app-admin",
-  GITHUB_ACCESS_TOKEN = "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"
+  GITHUB_ACCESS_TOKEN = "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"
 }
