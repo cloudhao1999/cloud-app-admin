f9034bc6dfb1d17961b12cf8e5fd292aa059dc14
cloudhao1999 <cyh19991002@gmail.com>
2022-05-30 10:09:00 +0800

    feat: 路由权限增加判断

--- src/router/autoload/index.ts
+++ src/router/autoload/index.ts
@@ -1,9 +1,18 @@
+import { userStore } from "@/store/user";
 import { Router, RouteRecordRaw } from "vue-router";
 import autoloadModuleRoutes from "./module";
 
-const routes: RouteRecordRaw[] = autoloadModuleRoutes();
+let routes: RouteRecordRaw[] = autoloadModuleRoutes();
 
 function autoload(router: Router) {
+  const user = userStore();
+  routes = routes.map((route) => {
+    route.children = route.children?.filter((r) => {
+      const permission = r.meta?.permission;
+      return permission ? user.info?.permissions.includes(permission) : true;
+    });
+    return route;
+  });
   routes.forEach((r) => router.addRoute(r));
 }
 
