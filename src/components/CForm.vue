dff788c42444f4f68f91bde187ce2083aa58c1c6
cloudhao1999 <cyh19991002@gmail.com>
2022-06-02 10:51:46 +0800

    feat: 自定义Form表单组件设计完成

---
+++ src/components/CForm.vue
@@ -0,0 +1,149 @@
+<script setup lang="ts">
+import { computed, ref, watch, watchEffect } from "vue";
+import { ElForm } from "element-plus";
+
+const types: Record<string, string> = {
+  input: "el-input",
+  select: "el-select",
+  inputNumber: "el-input-number",
+  timePicker: "el-time-picker",
+  datePicker: "el-date-picker"
+};
+
+const props = defineProps({
+  options: {
+    type: Array,
+    require: true
+  },
+  gutter: {
+    type: Number,
+    default: 24
+  },
+  colSpan: {
+    type: Object,
+    require: false
+  },
+  labelCol: {
+    type: Object,
+    require: false
+  },
+  wrapperCol: {
+    type: Object,
+    require: false
+  },
+  value: {
+    type: Object,
+    require: true,
+    default: () => {}
+  },
+  loading: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const emits = defineEmits(["submit", "update:value"]);
+const formRef = ref<InstanceType<typeof ElForm>>();
+const validatorRules = ref({});
+const formData = ref<any>({});
+const modelProps = ref<any>({});
+
+watch(
+  () => props.options as any,
+  (val: any[]) => {
+    const validatorRulesObj: any = {};
+    formData.value = val.map((item) => {
+      let { name, type, rules, tagName, ...rest } = item;
+      if (rules) {
+        validatorRulesObj[name] = rules;
+        if (types[type] && !tagName) {
+          tagName = types[type];
+        }
+        return {
+          ...rest,
+          name,
+          tagName
+        };
+      }
+    });
+    validatorRules.value = validatorRulesObj;
+  },
+  { immediate: true }
+);
+
+const computedColSpan = computed(() => {
+  const defaultColSpan =
+    props.options && props.options.length > 10 ? { md: 12, xs: 24 } : { span: 24 };
+  return props.colSpan || defaultColSpan;
+});
+
+async function submit() {
+  await formRef.value?.validate((valid: boolean) => {
+    if (valid) {
+      const values = props.value;
+      emits("submit", values);
+    }
+  });
+}
+
+function validateField(...args: any[]) {
+  formRef.value?.validateField(...args);
+}
+function validate(...args: any[]) {
+  formRef.value?.validate(...args);
+}
+function resetFields(...args: any[]) {
+  formRef.value?.resetFields(...args);
+}
+function clearValidate(...args: any[]) {
+  formRef.value?.clearValidate(...args);
+}
+defineExpose({
+  validateField,
+  validate,
+  resetFields,
+  clearValidate,
+  submit
+});
+
+watch(
+  () => props.value,
+  (val: any) => {
+    modelProps.value = val;
+  },
+  { immediate: true }
+);
+
+watchEffect(() => {
+  emits("update:value", modelProps.value);
+});
+</script>
+
+<template>
+  <div v-loading="loading">
+    <el-form ref="formRef" :rules="validatorRules" :model="value">
+      <el-row :gutter="gutter">
+        <template v-for="item in formData">
+          <el-col v-if="!item.hidden" v-bind="item.colSpan || computedColSpan" :key="item.name">
+            <el-form-item
+              :label-col="item.labelCol || labelCol"
+              :wrapper-col="item.wrapperCol || wrapperCol"
+              :prop="item.name"
+              :label="item.label"
+              v-bind="item.itemExtra"
+            >
+              <component
+                :is="item.tagName"
+                v-model="modelProps[item.name]"
+                v-bind="item.props"
+                v-on="item.on"
+              />
+            </el-form-item>
+          </el-col>
+        </template>
+      </el-row>
+    </el-form>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/design/modules/ArticleEditDialog.vue
+++ src/views/design/modules/ArticleEditDialog.vue
@@ -1,19 +1,12 @@
 <template>
   <el-dialog v-model="visible" :width="dialogWidth" :title="title">
-    <el-form ref="ruleFormRef" :rules="rules" :model="model">
-      <el-form-item label="标题" prop="title" :label-width="formLabelWidth">
-        <el-input v-model="model.title" autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="内容" prop="content" :label-width="formLabelWidth">
-        <el-input v-model="model.content" :autosize="{ minRows: 4, maxRows: 8 }" type="textarea" />
-      </el-form-item>
-    </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="visible = false">取消</el-button>
         <el-button type="primary" @click="submitForm(ruleFormRef)">确定</el-button>
       </span>
     </template>
+    <c-form ref="ruleFormRef" v-model:value="model" :options="options" />
   </el-dialog>
 </template>
 
@@ -21,21 +14,16 @@
 import { useMessage } from "@/hooks/useMessage";
 import { ArticleModel } from "@/model/article";
 import { useWindowSize } from "@vueuse/core";
-import { FormInstance, FormRules } from "element-plus";
-import { computed, reactive, ref } from "vue";
+import { FormInstance } from "element-plus";
+import { computed, ref } from "vue";
 
 const emit = defineEmits(["close"]);
 
 const visible = ref(false);
-const formLabelWidth = "80px";
 const ruleFormRef = ref<FormInstance>();
 const title = ref("新增");
 const { width } = useWindowSize();
 const model = ref<Partial<ArticleModel>>({});
-const rules = reactive<FormRules>({
-  title: [{ required: true, message: "请输入标题", trigger: "blur" }],
-  content: [{ required: true, message: "请输入内容", trigger: "blur" }]
-});
 
 const dialogWidth = computed(() => {
   return width.value > 768 ? "50%" : "80%";
@@ -55,6 +43,31 @@ async function submitForm(formEl: FormInstance | undefined) {
     }
   });
 }
+
+const options = [
+  {
+    name: "title",
+    type: "input",
+    label: "标题",
+    rules: [{ required: true, message: "请输入标题", trigger: "blur" }],
+    props: {
+      maxLength: 50,
+      placeholder: "请输入标题"
+    }
+  },
+  {
+    name: "content",
+    type: "input",
+    label: "内容",
+    rules: [{ required: true, message: "请输入内容！", trigger: "blur" }],
+    props: {
+      autosize: { minRows: 4, maxRows: 8 },
+      type: "textarea",
+      placeholder: "请输入内容"
+    }
+  }
+];
+
 defineExpose({
   edit,
   title: title
