2049e430e99531688538a7099139ac359fc94852
cloudhao1999 <cyh19991002@gmail.com>
2022-06-13 17:56:24 +0800

    fix: 修复iframe页面路由文件引用错误

--- src/layouts/iframe-page.vue
+++ src/layouts/iframe-page.vue
@@ -3,7 +3,7 @@ import Menu from "@/layouts/default/menu/index.vue";
 import Header from "@/layouts/default/header/index.vue";
 import History from "@/layouts/default/history/index.vue";
 import menuService from "@/hooks/useMenu";
-import IframePageView from "./iframe/index.vue";
+import IframePageView from "./pages/index.vue";
 import { useRoute } from "vue-router";
 import { watch } from "vue";
 
