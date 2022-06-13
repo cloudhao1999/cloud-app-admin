d93b7f9040e15bd76149ba95d78d9646b5afa1e9
cloudhao1999 <cyh19991002@gmail.com>
2022-06-13 16:19:22 +0800

    feat: 调整默认语言

--- src/plugins/i18n/index.ts
+++ src/plugins/i18n/index.ts
@@ -4,7 +4,7 @@ import messages from "@intlify/vite-plugin-vue-i18n/messages";
 
 export function setupI18n(app: App) {
   const i18n = createI18n({
-    locale: "en", // zh-CN
+    locale: "zh-CN", // zh-CN
     messages
   });
   app.use(i18n);
