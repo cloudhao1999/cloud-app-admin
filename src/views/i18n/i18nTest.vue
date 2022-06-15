79aa4f6d92e731006a1defb7c7f429c0f0c91067
cloudhao1999 <cyh19991002@gmail.com>
2022-06-15 17:56:35 +0800

    fix: 修复动画切换问题

--- src/views/i18n/i18nTest.vue
+++ src/views/i18n/i18nTest.vue
@@ -3,9 +3,11 @@ import i18nService from "@/hooks/useI18n";
 </script>
 
 <template>
-  <div class="p-5">
-    <span class="mr-4 text-gray-600">{{ i18nService.locale }}</span>
-    <el-color-picker :model-value="''" style="vertical-align: middle" />
+  <div class="relative w-full">
+    <div class="p-5 absolute w-full">
+      <span class="mr-4 text-gray-600">{{ i18nService.locale }}</span>
+      <el-color-picker :model-value="''" style="vertical-align: middle" />
+    </div>
   </div>
 </template>
 
