d602768714f6328238c42caab878ccf08c1d31ae
cloudhao1999 <cyh19991002@gmail.com>
2022-06-27 16:17:33 +0800

    chore: 首页排行榜table重构

--- src/views/dashboard/ActivityCard.vue
+++ src/views/dashboard/ActivityCard.vue
@@ -1,54 +1,10 @@
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
+import { setupDashobardAttributes } from "./modules/dashboard";
 
 const { t } = useI18n();
-const tableData = [
-  {
-    name: "可达鸭",
-    price: 50,
-    unit: "RMB",
-    category: "精神系",
-    color: "bg-yellow-500",
-    sort: "50%",
-    sortUnit: "up"
-  },
-  {
-    name: "皮卡丘",
-    price: 1000,
-    unit: "RMB",
-    category: "雷电系",
-    color: "bg-yellow-300",
-    sort: "120%",
-    sortUnit: "up"
-  },
-  {
-    name: "方酱",
-    price: 600,
-    unit: "RMB",
-    category: "草系",
-    color: "bg-green-500",
-    sort: "50%",
-    sortUnit: "down"
-  },
-  {
-    name: "杰尼龟",
-    price: 70,
-    unit: "RMB",
-    category: "鳄龟",
-    color: "bg-blue-400",
-    sort: "60%",
-    sortUnit: "up"
-  },
-  {
-    name: "波克比",
-    price: 1900,
-    unit: "RMB",
-    category: "精神系",
-    color: "bg-pink-400",
-    sort: "150%",
-    sortUnit: "up"
-  }
-];
+
+const { activityColumns, activityTableData } = setupDashobardAttributes();
 </script>
 
 <template>
@@ -56,46 +12,31 @@ const tableData = [
     <Card>
       <template #title> {{ t("page.common.dashboard.card.leaderboard") }} </template>
       <template #content>
-        <el-table :data="tableData" align="right" header-align="right" stripe style="width: 100%">
-          <el-table-column
-            prop="name"
-            :label="t('page.common.dashboard.column.leaderboard.name')"
-          />
-          <el-table-column
-            sortable
-            prop="price"
-            :label="t('page.common.dashboard.column.leaderboard.price')"
-            width="85"
-          />
-          <el-table-column
-            prop="category"
-            width="80"
-            :label="t('page.common.dashboard.column.leaderboard.tag')"
-          >
-            <template #default="scope">
-              <span class="p-1 rounded-md bg-gray-500 text-white text-sm">{{
-                scope.row.category
-              }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column
-            prop="sort"
-            width="110"
-            :label="t('page.common.dashboard.column.leaderboard.compared')"
-          >
-            <template #default="scope">
-              <div class="flex">
-                <i-mdi-trending-up
-                  v-if="scope.row.sortUnit === 'up'"
-                  class="text-red-500"
-                  style="font-size: 1.6em"
-                />
-                <i-mdi-trending-down v-else class="text-green-500" style="font-size: 1.6em" />
-                <span class="pl-2">{{ scope.row.sort }}</span>
-              </div>
-            </template>
-          </el-table-column>
-        </el-table>
+        <c-table
+          :table-data="activityTableData"
+          :columns="activityColumns"
+          align="right"
+          header-align="right"
+          stripe
+          style="width: 100%"
+        >
+          <template #category="scope">
+            <span class="p-1 rounded-md bg-gray-500 text-white text-sm">{{
+              scope.row.category
+            }}</span>
+          </template>
+          <template #sort="scope">
+            <div class="flex">
+              <i-mdi-trending-up
+                v-if="scope.row.sortUnit === 'up'"
+                class="text-red-500"
+                style="font-size: 1.6em"
+              />
+              <i-mdi-trending-down v-else class="text-green-500" style="font-size: 1.6em" />
+              <span class="pl-2">{{ scope.row.sort }}</span>
+            </div>
+          </template>
+        </c-table>
       </template>
     </Card>
   </div>
---
+++ src/views/dashboard/modules/dashboard.ts
@@ -0,0 +1,87 @@
+import { CTableColumn } from "#/table";
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
+
+export const setupDashobardAttributes = () => {
+  const { t } = useI18n();
+
+  const activityColumns = computed<CTableColumn<any>[]>(() => {
+    return [
+      {
+        prop: "name",
+        label: t("page.common.dashboard.column.leaderboard.name")
+      },
+      {
+        prop: "price",
+        sortable: true,
+        label: t("page.common.dashboard.column.leaderboard.price"),
+        width: "85"
+      },
+      {
+        prop: "category",
+        label: t("page.common.dashboard.column.leaderboard.tag"),
+        width: "80",
+        scoped: "category"
+      },
+      {
+        prop: "sort",
+        label: t("page.common.dashboard.column.leaderboard.compared"),
+        width: "110",
+        scoped: "sort"
+      }
+    ];
+  });
+
+  const activityTableData = [
+    {
+      name: "可达鸭",
+      price: 50,
+      unit: "RMB",
+      category: "精神系",
+      color: "bg-yellow-500",
+      sort: "50%",
+      sortUnit: "up"
+    },
+    {
+      name: "皮卡丘",
+      price: 1000,
+      unit: "RMB",
+      category: "雷电系",
+      color: "bg-yellow-300",
+      sort: "120%",
+      sortUnit: "up"
+    },
+    {
+      name: "方酱",
+      price: 600,
+      unit: "RMB",
+      category: "草系",
+      color: "bg-green-500",
+      sort: "50%",
+      sortUnit: "down"
+    },
+    {
+      name: "杰尼龟",
+      price: 70,
+      unit: "RMB",
+      category: "水系",
+      color: "bg-blue-400",
+      sort: "60%",
+      sortUnit: "up"
+    },
+    {
+      name: "波克比",
+      price: 1900,
+      unit: "RMB",
+      category: "精神系",
+      color: "bg-pink-400",
+      sort: "150%",
+      sortUnit: "up"
+    }
+  ];
+
+  return {
+    activityTableData,
+    activityColumns
+  };
+};
