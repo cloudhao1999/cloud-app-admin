f33a4b37008a830be93faca1a8887c81482fc99e
cloudhao1999 <cyh19991002@gmail.com>
2022-06-30 14:40:38 +0800

    chore: 部分样式调整

--- src/layouts/nested-page.vue
+++ src/layouts/nested-page.vue
@@ -7,7 +7,7 @@ const { t } = useI18n();
 <template>
   <el-container class="h-full w-full font-sans">
     <div class="w-full font-sans h-[250px] bg-gray-200 dark:bg-gray-600 m-1 rounded-lg border">
-      <p class="text-dark-200 text-lg m-2 font-bold">
+      <p class="text-dark-200 text-lg m-2 text-center font-bold">
         {{ t("page.common.nested.text.level1") }}
       </p>
       <PageView />
--- src/views/nested/Level.vue
+++ src/views/nested/Level.vue
@@ -5,8 +5,8 @@ const { t } = useI18n();
 </script>
 
 <template>
-  <div class="m-2 p-2 bg-gray-300 dark:bg-gray-800 h-[180px] rounded-lg border">
-    <span class="font-medium">{{ t("page.common.nested.text.level2") }}</span>
+  <div class="m-3 p-3 bg-gray-300 dark:bg-gray-800 h-[180px] rounded-lg border">
+    <p class="font-medium text-center">{{ t("page.common.nested.text.level2") }}</p>
   </div>
 </template>
 
