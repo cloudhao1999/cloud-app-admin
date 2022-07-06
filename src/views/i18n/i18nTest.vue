a3cc61464b6f96ab21e20fe86b3d7567c674acca
cloudhao1999 <cyh19991002@gmail.com>
2022-07-06 11:17:26 +0800

    fix: 解决element-ui组件报错

--- src/views/i18n/i18nTest.vue
+++ src/views/i18n/i18nTest.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
 import i18nService from "@/hooks/useI18n";
-
-const color = ref("#fff");
 </script>
 
 <template>
   <div class="relative w-full">
     <div class="p-5 absolute w-full">
       <span class="mr-4 text-gray-600">{{ i18nService.locale }}</span>
-      <el-color-picker v-model="color" style="vertical-align: middle" />
-      <p class="mt-2 mr-4 text-gray-600">{{ color }}</p>
+      <el-color-picker />
     </div>
   </div>
 </template>
