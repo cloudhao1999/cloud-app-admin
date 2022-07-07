9282ac87ee1d4ed6bd39baac5986cd28ba97dd9a
cloudhao1999 <cyh19991002@gmail.com>
2022-07-07 16:43:43 +0800

    chore: 去除无用的依赖

--- components.d.ts
+++ components.d.ts
@@ -59,7 +59,6 @@ declare module "@vue/runtime-core" {
     VeeValidateError: typeof import("./src/components/VeeValidateError.vue")["default"];
     WangEditor: typeof import("./src/components/WangEditor.vue")["default"];
   }
-
   export interface ComponentCustomProperties {
     vLoading: typeof import("element-plus/es")["ElLoadingDirective"];
   }
