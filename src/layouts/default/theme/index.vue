37cfff4a3fab30807cdecea28a5fb7b428215e3b
cloudhao1999 <cyh19991002@gmail.com>
2022-07-11 16:12:47 +0800

    chore: 合并部分逻辑

--- src/layouts/default/theme/index.vue
+++ src/layouts/default/theme/index.vue
@@ -5,24 +5,14 @@ const { next, state } = useTheme();
 </script>
 
 <template>
-  <i-mdi-white-balance-sunny
-    v-show="state === 'light' || state === 'auto' || state === ''"
-    class="dark-mode text-gray-500"
-    style="font-size: 1.6em"
-    @click="() => next()"
-  />
-  <i-mdi-weather-night
-    v-show="state === 'dark'"
-    class="dark-mode"
-    style="font-size: 1.6em"
-    @click="() => next()"
-  />
-  <i-mdi-apple-icloud
-    v-show="state === 'sky'"
-    class="dark-mode text-gray-600"
-    style="font-size: 1.6em"
-    @click="() => next()"
-  />
+  <div style="font-size: 1.6em" @click="() => next()">
+    <i-mdi-white-balance-sunny
+      v-show="state === 'light' || state === 'auto' || state === ''"
+      class="dark-mode text-gray-500"
+    />
+    <i-mdi-weather-night v-show="state === 'dark'" class="dark-mode" />
+    <i-mdi-apple-icloud v-show="state === 'sky'" class="dark-mode text-gray-600" />
+  </div>
 </template>
 
 <style scoped>
