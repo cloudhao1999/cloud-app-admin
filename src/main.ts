59a1002f28afe71f79ee0b4129a23c21ae871c6f
cloudhao1999 <cyh19991002@gmail.com>
2022-07-11 10:32:50 +0800

    fix: 修复组件重复注册的问题

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import { createApp } from "vue";
 import { setupPlugins } from "./plugins";
 import { setupRegisterGlobComp } from "./components";
 import { setupRouter } from "@/router";
-import AsyncSelect from "@/components/AsyncSelect.vue";
 
 import "@/style/base.scss";
 import App from "./App.vue";
 
 const app = createApp(App);
-app.component("AsyncSelect", AsyncSelect);
 
 setupPlugins(app);
 setupRegisterGlobComp(app);
