76e2a0e2791a221ce34b96d19e0224b84a76075c
cloudhao1999 <cyh19991002@gmail.com>
2022-07-14 20:21:27 +0800

    feat: vite版本升级至3.0，锁定vite-vue插件版本

--- src/router/autoload/module.ts
+++ src/router/autoload/module.ts
@@ -1,6 +1,11 @@
 import { RouteRecordRaw } from "vue-router";
 export default function autoloadModuleRoutes() {
-  const modules = import.meta.globEager("../module/**/*.ts");
+  const modules: Record<
+    string,
+    {
+      [key: string]: any;
+    }
+  > = import.meta.glob("../module/**/*.ts", { eager: true });
 
   const routes = [] as RouteRecordRaw[];
 
