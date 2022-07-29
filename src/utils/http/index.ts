f05f719391d16b13fb009628de0b8ded64733130
cloudhao1999 <cyh19991002@gmail.com>
2022-07-29 10:46:45 +0800

    chore: 调整post请求的参数

--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -85,14 +85,12 @@ class CloudHttp {
     method: RequestMethods,
     url: string,
     param?: AxiosRequestConfig,
-    data?: AxiosRequestConfig,
     axiosConfig?: CloudHttpRequestConfig
   ): Promise<T> {
     const config = {
       method,
       url,
       ...param,
-      ...data,
       ...axiosConfig
     } as CloudHttpRequestConfig;
 
@@ -110,8 +108,8 @@ class CloudHttp {
   }
 
   // 单独抽离的post工具函数
-  public post<T, P>(url: string, data?: T, config?: CloudHttpRequestConfig): Promise<P> {
-    return this.request<P>("post", url, data, config);
+  public post<T, P>(url: string, params?: T, config?: CloudHttpRequestConfig): Promise<P> {
+    return this.request<P>("post", url, params, config);
   }
 
   // 单独抽离的get工具函数
