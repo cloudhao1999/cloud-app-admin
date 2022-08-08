eb6b82ed422b3f29b46281606311f5816089e484
cloudhao1999 <cyh19991002@gmail.com>
2022-08-08 15:37:11 +0800

    chore: pinia写法迁移至setup方式，组织数据更加自然

--- src/store/user.ts
+++ src/store/user.ts
@@ -4,54 +4,58 @@ import { UserInfoModel } from "@/model/user";
 import { setToken } from "@/utils/auth";
 import { defineStore } from "pinia";
 
-export const userStore = defineStore("user", {
-  state: () => {
-    return {
-      info: {} as null | UserInfoModel
-    };
-  },
-  getters: {
-    name: (state) => {
-      return state.info?.name;
-    },
-    avatar: (state) => {
-      return state.info?.avatar;
-    },
-    isEmpty: (state) => {
-      return Object.keys(state.info!).length === 0;
-    },
-    permission: (state) => {
-      return state.info?.permission;
+export const userStore = defineStore("user", () => {
+  const info = ref<Partial<UserInfoModel>>({});
+
+  const name = computed(() => {
+    return info.value!.name;
+  });
+  const avatar = computed(() => {
+    return info.value!.avatar;
+  });
+  const isEmpty = computed(() => {
+    return Object.keys(info.value!).length === 0;
+  });
+  const permission = computed(() => {
+    return info.value?.permission;
+  });
+
+  async function getUserInfo() {
+    const permissions = permissionService.defaultPermission;
+    let data;
+    if (permissions.value === "admin") {
+      ({ data } = await fetchAdminInfo());
+    } else {
+      ({ data } = await fetchEditorInfo());
     }
-  },
-  actions: {
-    async getUserInfo() {
-      const permissions = permissionService.defaultPermission;
-      let data;
-      if (permissions.value === "admin") {
-        ({ data } = await fetchAdminInfo());
-      } else {
-        ({ data } = await fetchEditorInfo());
-      }
-      if (data) {
-        this.$state.info = data;
-      }
-    },
-    async login(loginParam: ILoginForm, success: () => void, error: (err: any) => void) {
-      try {
-        const {
-          data: { token }
-        } = await userLogin(loginParam);
-        if (token) {
-          setToken(token);
-          await this.getUserInfo();
-          success();
-        } else {
-          throw "获取token失败";
-        }
-      } catch (err) {
-        error(err);
-      }
+    if (data) {
+      info.value = data;
     }
   }
+  async function login(loginParam: ILoginForm, success: () => void, error: (err: any) => void) {
+    try {
+      const {
+        data: { token }
+      } = await userLogin(loginParam);
+      if (token) {
+        setToken(token);
+        await getUserInfo();
+        success();
+      } else {
+        throw "获取token失败";
+      }
+    } catch (err) {
+      error(err);
+    }
+  }
+
+  return {
+    info,
+    name,
+    avatar,
+    isEmpty,
+    permission,
+    login,
+    getUserInfo
+  };
 });
