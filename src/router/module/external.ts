83ed13e1c18a7360a0a5c1a2c840aaa5c3b11c5c
cloudhao1999 <cyh19991002@gmail.com>
2022-08-26 15:01:31 +0800

    feat: 增加博客、B站主页外部链接🔗

--- src/router/module/external.ts
+++ src/router/module/external.ts
@@ -11,6 +11,25 @@ export default {
       meta: {
         menu: { title: "router.github", path: "https://github.com/cloudhao1999/cloud-app-admin" }
       }
+    },
+    {
+      name: "Blog",
+      path: "blog",
+      component: () => import("@/layouts/empty-page.vue"),
+      meta: {
+        menu: { title: "router.blog", path: "https://www.cloudhao.top" }
+      }
+    },
+    {
+      name: "Bilibili",
+      path: "bilibili",
+      component: () => import("@/layouts/empty-page.vue"),
+      meta: {
+        menu: {
+          title: "router.bilibili",
+          path: "https://space.bilibili.com/28615318?spm_id_from=333.1007.0.0"
+        }
+      }
     }
   ]
 };
